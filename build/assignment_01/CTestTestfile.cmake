f05e0bfa96b7d268f6eb89c915219ce56cb0e7b0
mvallur <mvallur@g.clemson.edu>
2017-02-02 13:25:24 -0500

    Assignment_01

---
+++ build/assignment_01/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mkv/AUE893_mvallur/src/assignment_01
+# Build directory: /home/mkv/AUE893_mvallur/build/assignment_01
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mkv/AUE893_mvallur/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mkv/AUE893_mvallur/devel;/opt/ros/kinetic".split(';') if "/home/mkv/AUE893_mvallur/devel;/opt/ros/kinetic" != "" else []
